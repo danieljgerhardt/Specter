7ac4d1b1877f32df7bc536add4c7a496e33b7378
danieljgerhardt <danieljgerhardt@gmail.com>
2025-03-20 14:51:04 -0400

    added target spawner

---
+++ Source/Specter/CPPTargetSpawner.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPPTargetSpawner.h"
+
+// Sets default values
+ACPPTargetSpawner::ACPPTargetSpawner()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACPPTargetSpawner::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACPPTargetSpawner::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ACPPTargetSpawner::SpawnTarget(FVector Location)
+{
+	FRotator Rotation(0.0f, 0.0f, 0.0f); 
+	FActorSpawnParameters SpawnInfo;
+	GetWorld()->SpawnActor<AActor>(ActorToSpawn, Location, Rotation, SpawnInfo);
+}
+
+void ACPPTargetSpawner::InitSpawning(int Count)
+{
+	FVector Location = GetActorLocation();
+
+	for (int i = 0; i < Count; i++) {
+		FVector RandomVec = FMath::VRand();
+		RandomVec.X *= static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * SpawnRange.X;
+		RandomVec.Y *= static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * SpawnRange.Y;
+		RandomVec.Z *= static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * SpawnRange.Z;
+
+		RandomVec += Location;
+
+		SpawnTarget(RandomVec);
+	}
+}
+
---
+++ Source/Specter/CPPTargetSpawner.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CPPTargetSpawner.generated.h"
+
+UCLASS()
+class SPECTER_API ACPPTargetSpawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACPPTargetSpawner();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Spawning")
+	TSubclassOf<AActor> ActorToSpawn;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning")
+	FVector SpawnRange;
+
+	UFUNCTION(BlueprintCallable, Category = "Spawning")
+	void SpawnTarget(FVector Location);
+
+	UFUNCTION(BlueprintCallable, Category = "Spawning")
+	void InitSpawning(int Count);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+
+};
