8fbac5f42a40d8c22ad99c1e543d1dac0e2b97e5
danieljgerhardt <danieljgerhardt@gmail.com>
2025-04-10 10:28:44 -0400

    created cppblackhole for future transition

---
+++ Source/Specter/CPPBlackHole.cpp
@@ -0,0 +1,71 @@
+#include "CPPBlackHole.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+// Sets default values
+ACPPBlackHole::ACPPBlackHole()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Initialize the sphere component
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+	SphereComponent->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void ACPPBlackHole::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SphereComponent->SetSphereRadius(AffectingDistance);
+}
+
+// Called every frame
+void ACPPBlackHole::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	GetActorsWithCharacterMovement();
+
+	for (AActor* Actor : AffectedActors) {
+		FVector ActorLocation = Actor->GetActorLocation();
+		FVector BlackHoleLocation = GetActorLocation();
+		FVector PullDirection = (BlackHoleLocation - ActorLocation).GetSafeNormal();
+		float DistToActor = (BlackHoleLocation - ActorLocation).Size();
+		if (DistToActor > AffectingDistance) {
+			continue;
+		}
+
+		UCharacterMovementComponent* CharacterMovement = Actor->FindComponentByClass<UCharacterMovementComponent>();
+		if (CharacterMovement) {
+			//apply pull force
+			float DistanceScale = (PullForce - DistToActor);
+			FVector IterPullForce = PullDirection * DistanceScale;
+			CharacterMovement->AddImpulse(IterPullForce * DeltaTime, true);
+
+			//apply rotation force
+			FRotator UpdatedRotation = GetActorRotation() + RotationDirection;
+			FVector IterRotationDirection = FVector::CrossProduct(UKismetMathLibrary::GetUpVector(RotationDirection), PullDirection);
+			FVector IterRotationForce = IterRotationDirection.GetSafeNormal() * RotationForce;
+			CharacterMovement->AddImpulse(IterRotationForce * DeltaTime, true);
+			SetActorRotation(UpdatedRotation);
+		}
+	}
+}
+
+void ACPPBlackHole::GetActorsWithCharacterMovement()
+{
+	TArray<AActor*> OverlappingActors;
+	SphereComponent->GetOverlappingActors(OverlappingActors);
+
+	AffectedActors.Empty();
+	for (AActor* Actor : OverlappingActors)
+	{
+		if (Actor->FindComponentByClass<UCharacterMovementComponent>())
+		{
+			AffectedActors.Add(Actor);
+		}
+	}
+}
---
+++ Source/Specter/CPPBlackHole.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CPPBlackHole.generated.h"
+
+UCLASS()
+class SPECTER_API ACPPBlackHole : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACPPBlackHole();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Black Hole")
+	FRotator RotationDirection{ 0.0f, 0.0f, 1.0f };
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Black Hole")
+	float PullForce{ 1000.0f };
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Black Hole")
+	float AffectingDistance{ 1000.0f };
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Black Hole")
+	float RotationForce{ 2000.0f };
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Black Hole")
+	TArray<AActor*> AffectedActors;
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	class USphereComponent* SphereComponent;
+
+	void GetActorsWithCharacterMovement();
+
+};
